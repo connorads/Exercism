ffb5616177f3007331e9d932a9de6551b0ba12d3
Connor Adams <10026538+connorads@users.noreply.github.com>
2019-10-26 12:58:22 +0100

    Raindrops first submission

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,8 @@
+﻿module Raindrops
+
+let convert (number: int): string =   
+    [ (3, "Pling"); (5, "Plang"); (7, "Plong") ]
+    |> List.filter (fun (n,_) -> number % n = 0 )
+    |> function
+       | [] -> string number
+       | list -> list |> List.fold (fun acc (_,sound) -> acc + sound) ""
