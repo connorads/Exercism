c5b1042a22b1d6d3cafd599db935d7632136a5c0
Connor Adams <10026538+connorads@users.noreply.github.com>
2019-10-28 00:14:16 +0000

    Accumulate first submission

---
+++ fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,9 @@
+﻿module Accumulate
+
+let accumulate func input =
+    let cons x y = List.Cons (x,y)
+    let rec map f acc list =
+        match list with
+        | [] -> acc []
+        | head::tail -> map f (acc << (cons (f head))) tail
+    map func id input
