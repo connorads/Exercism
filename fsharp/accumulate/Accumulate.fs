a58403806690dfef748b6d77c7ee355d3e88e378
Connor Adams <10026538+connorads@users.noreply.github.com>
2019-11-01 20:47:46 +0000

    Play around with different implementations to debug and test tail recursion

--- fsharp/accumulate/Accumulate.fs
+++ fsharp/accumulate/Accumulate.fs
@@ -1,7 +1,32 @@
 ﻿module Accumulate
 
-let accumulate func input =
-    let rec map f acc = function
-        | [] -> acc
-        | head::tail -> map f (f head :: acc) tail
-    map func [] input |> List.rev
+//let accumulate func input =
+//    let rec map f acc = function
+//        | [] -> acc
+//        | head::tail -> map f (f head :: acc) tail
+//    map func [] input |> List.rev
+    
+//let rec accumulate f = function
+//    | [] -> []
+//    | h::t -> f h :: accumulate f t
+    
+let accumulate f input =
+    let rec acc f = function
+        | [] -> []
+        | h::t -> f h :: acc f t
+    acc f input
+
+//let accumulate f xs =
+//    let cons x xs = x :: xs
+//    let rec mapImp f acc = function
+//        | [] -> acc []
+//        | h::t -> mapImp f (acc << (cons (f h))) t
+//    mapImp f id xs
+    
+//let accumulate func input =
+//    let cons x y = List.Cons (x,y)
+//    let rec map f acc list =
+//        match list with
+//        | [] -> acc []
+//        | head::tail -> map f (acc << (cons (f head))) tail
+//    map func id input
