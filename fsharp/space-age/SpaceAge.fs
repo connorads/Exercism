0e3ecf703538f6f1f3f131cbc05b3cdd24862fc6
Connor Adams <10026538+connorads@users.noreply.github.com>
2019-11-10 18:50:18 +0000

    SpaceAge first submission

---
+++ fsharp/space-age/SpaceAge.fs
@@ -0,0 +1,24 @@
+﻿module SpaceAge
+
+type Planet =
+    | Earth
+    | Mercury
+    | Venus
+    | Mars
+    | Jupiter
+    | Saturn
+    | Uranus
+    | Neptune
+
+let age (planet: Planet) (seconds: int64): float =
+    let oneEarthYearInSeconds = (float) 31557600
+    let secondsInEarthYears = (float) seconds / oneEarthYearInSeconds
+    match planet with
+    | Earth -> secondsInEarthYears
+    | Mercury -> secondsInEarthYears / 0.2408467
+    | Venus -> secondsInEarthYears / 0.61519726
+    | Mars -> secondsInEarthYears / 1.8808158
+    | Jupiter -> secondsInEarthYears / 11.862615
+    | Saturn -> secondsInEarthYears / 29.447498
+    | Uranus -> secondsInEarthYears / 84.016846
+    | Neptune -> secondsInEarthYears / 164.79132
