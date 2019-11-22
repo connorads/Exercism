2e9544c3307e79b674846b03522946b9fa3f4848
Connor Adams <10026538+connorads@users.noreply.github.com>
2019-11-22 20:44:19 +0000

    GradeSchool first submission

---
+++ fsharp/grade-school/GradeSchool.fs
@@ -0,0 +1,18 @@
+﻿module GradeSchool
+
+type School = Map<int, string list>
+
+let empty: School = Map.empty<int, string list>
+
+let add (student: string) (grade: int) (school: School): School =
+    match school.TryFind(grade) with
+    | None -> school.Add(grade, [student])
+    | Some students -> school.Add(grade, (student :: students))
+
+let roster (school: School): string list =
+    school |> Map.toSeq |> Seq.sortBy fst |> Seq.map snd |> Seq.map (fun x -> x |> List.sort) |> List.concat
+
+let grade (number: int) (school: School): string list =
+    match school.TryFind(number) with
+    | None -> []
+    | Some students -> students |> List.sort
