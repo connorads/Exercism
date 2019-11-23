8be48aa925404bcfad207b579e81f40587437b1e
Connor Adams <10026538+connorads@users.noreply.github.com>
2019-11-23 13:51:57 +0000

    ErrorHandling first submission

---
+++ fsharp/error-handling/ErrorHandling.fs
@@ -0,0 +1,22 @@
+﻿module ErrorHandling
+open System
+open System
+
+let handleErrorByThrowingException() = failwith "Oh noes"
+
+let handleErrorByReturningOption (input: string) =
+    try Some (int input)
+    with | _ -> None
+
+let handleErrorByReturningResult (input: string) =
+    try Ok (int input)
+    with | _ -> Error "Could not convert input to integer"
+
+let bind switchFunction twoTrackInput =
+    twoTrackInput |> Result.bind switchFunction
+
+let cleanupDisposablesWhenThrowingException (resource: IDisposable) =
+    try
+        failwith "Oh noes"
+    finally
+        resource.Dispose()
