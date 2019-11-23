0b4e2ed6c32d29f3c5ed58449f4b33c6d8289ddc
Connor Adams <10026538+connorads@users.noreply.github.com>
2019-11-23 00:26:59 +0000

    GradeSchool second submission

--- fsharp/grade-school/GradeSchool.fs
+++ fsharp/grade-school/GradeSchool.fs
@@ -5,14 +5,20 @@ type School = Map<int, string list>
 let empty: School = Map.empty<int, string list>
 
 let add (student: string) (grade: int) (school: School): School =
-    match school.TryFind(grade) with
-    | None -> school.Add(grade, [student])
-    | Some students -> school.Add(grade, (student :: students))
+    school
+    |> Map.tryFind grade
+    |> Option.map (fun students -> student :: students)
+    |> Option.defaultValue [student]
+    |> Map.add grade <| school
 
 let roster (school: School): string list =
-    school |> Map.toSeq |> Seq.sortBy fst |> Seq.map snd |> Seq.map (fun x -> x |> List.sort) |> List.concat
+    school
+    |> Map.toSeq
+    |> Seq.sortBy fst
+    |> Seq.map (snd >> List.sort) 
+    |> List.concat
 
 let grade (number: int) (school: School): string list =
-    match school.TryFind(number) with
-    | None -> []
-    | Some students -> students |> List.sort
+    Map.tryFind number school
+    |> Option.map List.sort
+    |> Option.defaultValue []
