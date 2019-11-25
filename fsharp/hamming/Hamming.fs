fa0887562d220b3984e82dac23960d082204bd60
Connor Adams <10026538+connorads@users.noreply.github.com>
2019-11-25 09:23:32 +0000

    Hamming second submission

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -1,9 +1,11 @@
 ﻿module Hamming
 
 let distance (strand1: string) (strand2: string): int option =
-    if strand1.Length <> strand2.Length then None else
-    (strand1, strand2)
-    ||> Seq.zip
-    |> Seq.map (fun (s1, s2) -> if s1 = s2 then 0 else 1)
-    |> Seq.sum
-    |> Some
+    if strand1.Length <> strand2.Length
+    then None
+    else
+        (strand1, strand2)
+        ||> Seq.zip
+        |> Seq.map (fun (s1, s2) -> if s1 = s2 then 0 else 1)
+        |> Seq.sum
+        |> Some
