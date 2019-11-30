26846cc3d4ebaf4157764feee6ab895c30df75e1
Connor Adams <10026538+connorads@users.noreply.github.com>
2019-11-30 22:04:46 +0000

    Clock third submission

--- fsharp/clock/Clock.fs
+++ fsharp/clock/Clock.fs
@@ -2,30 +2,29 @@ module Clock
 
 let normaliseHours hours = ((hours % 24) + 24) % 24
 
-let (|NegativeMinutesAndHours|NegativeMinutes|NegativeHours|PositiveMinutesAndHours|) (h, m) =
-    if m < 0 && h < 0 then NegativeMinutesAndHours
-    elif m < 0 then NegativeMinutes
-    elif h < 0 then NegativeHours
-    else PositiveMinutesAndHours
+let (|IsNegative|_|) x = if x < 0 then Some() else None
 
 let toHoursAndMinutes minutes =
     let hours = minutes / 60
     let mins = minutes % 60
+    
     match (hours, mins) with
-    | NegativeMinutesAndHours -> (hours + 23, 60 + mins)
-    | NegativeMinutes -> ((hours + 23) % 24, 60 + mins)
-    | NegativeHours -> (hours + 24, mins)
-    | PositiveMinutesAndHours -> (hours, mins)
+    | IsNegative, IsNegative -> (hours + 23, 60 + mins)
+    | _, IsNegative -> ((hours + 23) % 24, 60 + mins)
+    | IsNegative, _ -> (hours + 24, mins)
+    | _ -> (hours, mins)
 
 let create hours minutes =
     let (extraHours, newMinutes) = toHoursAndMinutes minutes
     let newHours = normaliseHours (hours + extraHours)
+    
     (newHours, newMinutes)
 
 let add minutes clock =
     let (existingHours, existingMinutes) = clock
     let (extraHours, newMinutes) = toHoursAndMinutes (existingMinutes + minutes)
     let newHours = normaliseHours (existingHours + extraHours)
+    
     (newHours, newMinutes)
 
 let subtract minutes clock =
