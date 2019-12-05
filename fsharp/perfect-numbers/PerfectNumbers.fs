a1e39b8e2eea670d2d574a44b90090071f97c3ed
Connor Adams <10026538+connorads@users.noreply.github.com>
2019-12-05 00:23:45 +0000

    PerfectNumbers third submission

--- fsharp/perfect-numbers/PerfectNumbers.fs
+++ fsharp/perfect-numbers/PerfectNumbers.fs
@@ -3,14 +3,14 @@
 type Classification = Perfect | Abundant | Deficient 
 
 let classify n : Classification option =
-    match n with
-    | num when num < 1 -> None
-    | num when num = 1 -> Some Deficient
-    | _ ->
-        seq { 1 .. n/2 }
-        |> Seq.where (fun i -> n % i = 0)
-        |> Seq.sum
+    if n <= 0
+    then None
+    else
+        [ 1 .. n/2 ]
+        |> List.where (fun i -> n % i = 0)
+        |> List.sum
         |> function
-        | sum when sum > n -> Some Abundant
-        | sum when sum < n -> Some Deficient
-        | _ -> Some Perfect
+            | sum when sum > n -> Abundant
+            | sum when sum < n -> Deficient
+            | _ -> Perfect
+        |> Some
