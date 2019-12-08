c25ad93282f944e3d4a6b263163b1d6eb0979cdb
Connor Adams <10026538+connorads@users.noreply.github.com>
2019-12-08 22:39:14 +0000

    Bob first submission

---
+++ fsharp/bob/Bob.fs
@@ -0,0 +1,18 @@
+﻿module Bob
+
+open System.Text.RegularExpressions
+
+let response (input: string): string =
+    let yelling = input = input.ToUpper() && Regex.IsMatch(input, @"[a-zA-Z]")
+    let silence = input.Replace(@"\r\n", "").Trim() = ""
+    let question = input.TrimEnd().EndsWith("?")
+    
+    if silence
+    then "Fine. Be that way!"
+    else if question
+         then if yelling
+              then "Calm down, I know what I'm doing!"
+              else "Sure."
+         else if yelling
+              then "Whoa, chill out!"
+              else "Whatever."
