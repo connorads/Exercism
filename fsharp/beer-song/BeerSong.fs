b517959dd6b75bd18b0d50f9345d880090ad39fd
Connor Adams <10026538+connorads@users.noreply.github.com>
2019-12-23 22:16:23 +0000

    BeerSong third submission

--- fsharp/beer-song/BeerSong.fs
+++ fsharp/beer-song/BeerSong.fs
@@ -1,34 +1,35 @@
 ﻿module BeerSong
 
 let bottleWord numberOfBottles =
-        if numberOfBottles = 1
-        then "bottle"
-        else "bottles"
+    if numberOfBottles = 1
+    then "bottle"
+    else "bottles"
 
 let quantityWord numberOfBottles =
-        if numberOfBottles = 0
-        then "No more"
-        else string numberOfBottles
+    if numberOfBottles = 0
+    then "No more"
+    else string numberOfBottles
     
 let firstSentence numberOfBottles = 
-        let quantity = quantityWord numberOfBottles
-        let bottle = bottleWord numberOfBottles
-        
-        sprintf "%s %s of beer on the wall, %s %s of beer." quantity bottle (quantity.ToLower()) bottle
+    let quantity = quantityWord numberOfBottles
+    let bottle = bottleWord numberOfBottles
+    
+    sprintf "%s %s of beer on the wall, %s %s of beer." quantity bottle (quantity.ToLower()) bottle
     
 let secondSentence numberOfBottles =
-        let nextNumberOfBottles = numberOfBottles - 1
-        let bottlePronoun = if nextNumberOfBottles = 0 then "it" else "one"
-        let quantity = quantityWord nextNumberOfBottles
-        let bottle = bottleWord nextNumberOfBottles
+    let nextNumberOfBottles = numberOfBottles - 1
+    let bottlePronoun = if nextNumberOfBottles = 0 then "it" else "one"
+    let quantity = quantityWord nextNumberOfBottles
+    let bottle = bottleWord nextNumberOfBottles
        
-        if nextNumberOfBottles >= 0
-        then sprintf "Take %s down and pass it around, %s %s of beer on the wall." bottlePronoun (quantity.ToLower()) bottle
-        else "Go to the store and buy some more, 99 bottles of beer on the wall."
+    if nextNumberOfBottles >= 0
+    then sprintf "Take %s down and pass it around, %s %s of beer on the wall." bottlePronoun (quantity.ToLower()) bottle
+    else "Go to the store and buy some more, 99 bottles of beer on the wall."
 
 let recite (startBottles: int) (takeDown: int) =
     let verse bottle = [ ""; firstSentence bottle; secondSentence bottle ]
     let endBottles = startBottles - takeDown + 1
+    
     [startBottles .. -1 .. endBottles]
     |> List.collect verse 
     |> List.tail
