40760322a9b943ac2721703b084501e503b04b0c
Connor Adams <10026538+connorads@users.noreply.github.com>
2020-01-11 23:31:54 +0000

    Allergies first submission

---
+++ fsharp/allergies/Allergies.fs
@@ -0,0 +1,23 @@
+﻿module Allergies
+
+open System
+
+[<Flags>]
+type Allergen =
+    | Eggs = 1
+    | Peanuts = 2
+    | Shellfish = 4
+    | Strawberries = 8
+    | Tomatoes = 16
+    | Chocolate = 32
+    | Pollen = 64
+    | Cats = 128
+
+let allergicTo codedAllergies allergen =
+    int allergen &&& codedAllergies > 0
+
+let list codedAllergies =
+    Enum.GetValues typeof<Allergen>
+    |> Seq.cast<Allergen>
+    |> Seq.filter (allergicTo codedAllergies)
+    |> Seq.toList
