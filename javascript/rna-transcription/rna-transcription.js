9790a63765ccdcc1e7f27f8af9b4414dec83f930
Connor Adams <10026538+connorads@users.noreply.github.com>
2020-01-11 18:55:40 +0000

    RNATranscription third submission

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,10 +1,14 @@
-export const toRna = (dna) => {
-  return Array.from(dna).map(nucleotide => {
-    switch(nucleotide) {
-      case 'G': return 'C';
-      case 'C': return 'G';
-      case 'T': return 'A';
-      case 'A': return 'U';
-    }
-  }).join("");
+export const toRna = dna => {
+  return Array.from(dna)
+    .map(nucleotide => {
+      return dnaToRnaMap.get(nucleotide);
+    })
+    .join("");
 };
+
+const dnaToRnaMap = new Map([
+  ["G", "C"],
+  ["C", "G"],
+  ["T", "A"],
+  ["A", "U"]
+]);
