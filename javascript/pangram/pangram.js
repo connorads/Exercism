1df7efa4179b51720ca82e028d848f1a1a5ebdf1
Connor Adams <10026538+connorads@users.noreply.github.com>
2020-01-12 21:50:52 +0000

    Pangram second submission

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,8 +1,6 @@
 export const isPangram = input => {
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  const inputLowercase = input.toLowerCase();
-  for (let i = 0; i < alphabet.length; i++) {
-    if (inputLowercase.indexOf(alphabet[i]) == -1) return false;
-  }
-  return true;
+  input = input.toLowerCase();
+  return [..."abcdefghijklmnopqrstuvwxyz"].every(letter =>
+    input.includes(letter)
+  );
 };
