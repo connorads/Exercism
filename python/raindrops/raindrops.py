e53e3b9c7882c07624374faeae7a113e5a7a1e7b
Connor Adams <10026538+connorads@users.noreply.github.com>
2020-03-08 22:25:20 +0000

    Raindrops second submission

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,4 +1,4 @@
 def convert(number):
-    raindrops = [v for k, v in {
-        3: "Pling", 5: "Plang", 7: "Plong"}.items() if number % k == 0]
+    raindrops = [sound for factor, sound in {
+        3: "Pling", 5: "Plang", 7: "Plong"}.items() if number % factor == 0]
     return "".join(raindrops) if raindrops else str(number)
