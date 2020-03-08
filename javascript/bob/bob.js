f3e4705513e5cb5f409a72bbfd4451389097bccb
Connor Adams <10026538+connorads@users.noreply.github.com>
2020-03-08 11:15:41 +0100

    Bob third submission

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,11 +1,14 @@
 export const hey = message => {
-  const yelling = message == message.toUpperCase() && message.match(/[a-z]/i);
   const silence = message.trim() == "";
-  const question = message.trimEnd().endsWith("?");
 
   if (silence) {
     return "Fine. Be that way!";
-  } else if (question && yelling) {
+  }
+
+  const yelling = message == message.toUpperCase() && message.match(/[a-z]/i);
+  const question = message.trimEnd().endsWith("?");
+
+  if (question && yelling) {
     return "Calm down, I know what I'm doing!";
   } else if (question) {
     return "Sure.";
