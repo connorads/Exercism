170d0bc80377bd8391b76ba90dc3cd86d82a5b94
Connor Adams <connorads@users.noreply.github.com>
2020-12-26 14:58:58 +0000

    RnaTranscription second submission

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -10,10 +10,9 @@ defmodule RnaTranscription do
 
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    dna_to_rna_map = %{'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
+    dna_to_rna_map = %{?G => ?C, ?C => ?G, ?T => ?A, ?A => ?U}
 
     dna
-    |> Enum.map(fn nucleotide -> Map.get(dna_to_rna_map, [nucleotide]) end)
-    |> List.flatten()
+    |> Enum.map(&Map.get(dna_to_rna_map, &1))
   end
 end
