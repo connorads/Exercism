fc51413910ef127392c6f869be10d04c6854b451
Connor Adams <connorads@users.noreply.github.com>
2021-01-03 14:23:42 +0000

    ArmstrongNumbers second submission

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,44 +1,22 @@
 #include "armstrong_numbers.h"
 #include <math.h>
 
-#define INT_MAX_NUMBER_OF_DIGITS 10
-
-typedef struct Digits
-{
-    int array[INT_MAX_NUMBER_OF_DIGITS];
-    int count;
-} Digits;
-
-static Digits get_digits(int number)
-{
-    Digits digits;
-    int i = 0;
-    while (number > 0)
-    {
-        int digit = number % 10;
-        digits.array[i] = digit;
-        number /= 10;
-        i++;
-    }
-    digits.count = i;
-    return digits;
-}
-
-static int sum_of_digits_each_raised_to_the_power_of_the_number_of_digits(struct Digits digits)
-{
-    int sum = 0;
-    for (int i = 0; i < digits.count; i++)
-    {
-        sum += pow(digits.array[i], digits.count);
-    }
-    return sum;
-}
-
 bool is_armstrong_number(int candidate)
 {
-    Digits digits = get_digits(candidate);
+    int count = 0, n = candidate;
+    while (n > 0)
+    {
+        n /= 10;
+        ++count;
+    }
 
-    int sum = sum_of_digits_each_raised_to_the_power_of_the_number_of_digits(digits);
+    int sum = 0;
+    n = candidate;
+    while (n > 0)
+    {
+        sum += pow(n % 10, count);
+        n /= 10;
+    }
 
     return sum == candidate;
 }
