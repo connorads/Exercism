01392b282c3177a034e22b8567ec81d63f2d5e2c
Connor Adams <connorads@users.noreply.github.com>
2021-01-04 20:20:49 +0000

    ResistorColor second submission

--- c/resistor-color/src/resistor_color.c
+++ c/resistor-color/src/resistor_color.c
@@ -5,8 +5,7 @@ int color_code(resistor_band_t color)
     return color;
 }
 
-resistor_band_t codes[] =
-    {BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE};
+resistor_band_t codes[] = COLORS;
 
 resistor_band_t *colors(void)
 {
--- c/resistor-color/src/resistor_color.h
+++ c/resistor-color/src/resistor_color.h
@@ -1,19 +1,12 @@
 #ifndef RESISTOR_COLOR_H
 #define RESISTOR_COLOR_H
 
-typedef enum
-{
-    BLACK,
-    BROWN,
-    RED,
-    ORANGE,
-    YELLOW,
-    GREEN,
-    BLUE,
-    VIOLET,
-    GREY,
-    WHITE
-} resistor_band_t;
+#define COLORS                                                              \
+    {                                                                       \
+        BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE \
+    }
+
+typedef enum COLORS resistor_band_t;
 
 int color_code(resistor_band_t color);
 
