b8272f75ddd66be901b53e80b2ae7b11fb9bc683
Connor Adams <connorads@users.noreply.github.com>
2021-01-05 22:16:32 +0000

    Pangram second submission

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,11 +1,8 @@
 module Pangram (isPangram) where
 
 import Data.Char (toLower)
-import Prelude
-
-toLowerString :: String -> String
-toLowerString = map toLower
 
 isPangram :: String -> Bool
 isPangram text =
-  all (`elem` toLowerString text) ['a' .. 'z']
+  let lowerText = map toLower text
+   in all (`elem` lowerText) ['a' .. 'z']
