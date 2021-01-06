8cfed7e72c88353eeda40e78750c84ec2a47f2dd
Connor Adams <connorads@users.noreply.github.com>
2021-01-06 22:58:53 +0000

    TwoFer first submission

---
+++ clojure/two-fer/project.clj
@@ -0,0 +1,6 @@
+(defproject two-fer "0.1.0-SNAPSHOT"
+  :description "two-fer exercise."
+  :url "https://github.com/exercism/clojure/tree/master/exercises/two-fer"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-cljfmt "0.7.0"]])
+
