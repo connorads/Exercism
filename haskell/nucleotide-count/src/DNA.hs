91a42c608c08e548c7a20379f2ea4b481f092ddc
Connor Adams <connorads@users.noreply.github.com>
2021-01-31 19:08:18 +0000

    NucleotideCounts second submission

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,24 +1,19 @@
 module DNA (nucleotideCounts, Nucleotide (..)) where
 
+import Control.Monad (foldM)
 import Data.Map (Map, adjust, fromList)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
+toNucleotide :: Char -> Either String Nucleotide
+toNucleotide 'A' = Right A
+toNucleotide 'C' = Right C
+toNucleotide 'G' = Right G
+toNucleotide 'T' = Right T
+toNucleotide c = Left ("Cannot convert '" ++ [c] ++ "' to Nucleotide")
+
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts "" = Right (fromList [(A, 0), (C, 0), (G, 0), (T, 0)])
-nucleotideCounts (x : xs) =
-  either
-    Left
-    ( \m ->
-        maybe
-          (Left ("Cannot convert '" ++ [x] ++ "' to Nucleotide"))
-          (\n -> Right (adjust (+ 1) n m))
-          (toNucleotide x)
-    )
-    (nucleotideCounts xs)
-  where
-    toNucleotide 'A' = Just A
-    toNucleotide 'C' = Just C
-    toNucleotide 'G' = Just G
-    toNucleotide 'T' = Just T
-    toNucleotide _ = Nothing
+nucleotideCounts =
+  foldM
+    (\m c -> fmap (\n -> adjust (+ 1) n m) (toNucleotide c))
+    (fromList [(A, 0), (C, 0), (G, 0), (T, 0)])
