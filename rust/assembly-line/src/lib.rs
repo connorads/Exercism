cd15f3c17587ef823176ded9b8ad647f883f7b6b
Connor Adams <connorads@users.noreply.github.com>
2022-09-07 21:47:20 +0800

    AssemblyLine first submission

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,13 +1,20 @@
-pub fn production_rate_per_hour(speed: u8) -> f64 {
-    return match speed {
+const CAR_PRODUCTION_BASE_RATE: f64 = 221.0;
+const MINS_IN_HOUR: u32 = 60;
+
+fn success_rate(speed: u8) -> f64 {
+    match speed {
         0 => 0.0,
-        1 | 2 | 3 | 4 => speed as f64 * 221.0,
-        5 | 6 | 7 | 8 => 0.9 * speed as f64 * 221.0,
-        9 | 10 => 0.77 * speed as f64 * 221.0,
+        1..=4 => 1.0,
+        5..=8 => 0.9,
+        9..=10 => 0.77,
         11..=u8::MAX => todo!(),
-    };
+    }
+}
+
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    success_rate(speed) * CAR_PRODUCTION_BASE_RATE * speed as f64
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    return (production_rate_per_hour(speed) / 60.0) as u32;
+    production_rate_per_hour(speed) as u32 / MINS_IN_HOUR
 }
