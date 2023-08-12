6af53adb4bf4f846dae2e0acb4f167cabb098aca
Connor Adams <connorads@users.noreply.github.com>
2023-08-12 16:30:44 +0200

    TisburyTreasureHunt first submission

---
+++ python/tisbury-treasure-hunt/tuples.py
@@ -0,0 +1,76 @@
+"""Functions to help Azara and Rui locate pirate treasure."""
+
+AzaraRecord = tuple[str, str]
+CoordinateTuple = tuple[str, str]
+RuiRecord = tuple[str, tuple[str, str], str]
+CombinedRecord = tuple[AzaraRecord, RuiRecord]
+CombinedRecordGroup = tuple[CombinedRecord, ...]
+
+
+def get_coordinate(record: AzaraRecord) -> str:
+    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
+
+    :param record: tuple - with a (treasure, coordinate) pair.
+    :return: str - the extracted map coordinate.
+    """
+
+    return record[1]
+
+
+def convert_coordinate(coordinate: str) -> CoordinateTuple:
+    """Split the given coordinate into tuple containing its individual components.
+
+    :param coordinate: str - a string map coordinate
+    :return: tuple - the string coordinate split into its individual components.
+    """
+
+    return (coordinate[0], coordinate[1])
+
+
+def compare_records(azara_record: AzaraRecord, rui_record: RuiRecord) -> bool:
+    """Compare two record types and determine if their coordinates match.
+
+    :param azara_record: tuple - a (treasure, coordinate) pair.
+    :param rui_record: tuple - a (location, tuple(coordinate_1, coordinate_2), quadrant) trio.
+    :return: bool - do the coordinates match?
+    """
+
+    if rui_record[1] == convert_coordinate(get_coordinate(azara_record)):
+        return True
+    else:
+        return False
+
+
+def create_record(
+    azara_record: AzaraRecord, rui_record: RuiRecord
+) -> CombinedRecord | str:
+    """Combine the two record types (if possible) and create a combined record group.
+
+    :param azara_record: tuple - a (treasure, coordinate) pair.
+    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
+    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
+    """
+
+    if compare_records(azara_record, rui_record):
+        return azara_record + rui_record
+    else:
+        return "not a match"
+
+
+def clean_up(combined_record_group: CombinedRecordGroup) -> str:
+    """Clean up a combined record group into a multi-line string of single records.
+
+    :param combined_record_group: tuple - everything from both participants.
+    :return: str - everything "cleaned", excess coordinates and information are removed.
+
+    The return statement should be a multi-lined string with items separated by newlines.
+
+    (see HINTS.md for an example).
+    """
+
+    return "".join(
+        map(
+            lambda record: str((record[0], record[2], record[3], record[4])) + "\n",
+            combined_record_group,
+        )
+    )
