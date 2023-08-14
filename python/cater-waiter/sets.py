23532027c068a02f737852567f8c3a153bf863d3
Connor Adams <connorads@users.noreply.github.com>
2023-08-14 15:57:47 +0200

    CaterWaiter first submission

---
+++ python/cater-waiter/sets.py
@@ -0,0 +1,142 @@
+"""Functions for compiling dishes and ingredients for a catering company."""
+
+
+from sets_categories_data import (
+    VEGAN,
+    VEGETARIAN,
+    KETO,
+    PALEO,
+    OMNIVORE,
+    ALCOHOLS,
+    SPECIAL_INGREDIENTS,
+)
+
+
+def clean_ingredients(
+    dish_name: str, dish_ingredients: list[str]
+) -> tuple[str, set[str]]:
+    """Remove duplicates from `dish_ingredients`.
+
+    :param dish_name: str - containing the dish name.
+    :param dish_ingredients: list - dish ingredients.
+    :return: tuple - containing (dish_name, ingredient set).
+
+    This function should return a `tuple` with the name of the dish as the first item,
+    followed by the de-duped `set` of ingredients as the second item.
+    """
+
+    return dish_name, set(dish_ingredients)
+
+
+def check_drinks(drink_name: str, drink_ingredients: list[str]) -> str:
+    """Append "Cocktail" (alcohol)  or "Mocktail" (no alcohol) to `drink_name`, based on `drink_ingredients`.
+
+    :param drink_name: str - name of the drink.
+    :param drink_ingredients: list - ingredients in the drink.
+    :return: str - drink_name appended with "Mocktail" or "Cocktail".
+
+    The function should return the name of the drink followed by "Mocktail" (non-alcoholic) and drink
+    name followed by "Cocktail" (includes alcohol).
+
+    """
+
+    is_cocktail = any(ingredient in ALCOHOLS for ingredient in drink_ingredients)
+    return f"{drink_name} {['Mocktail', 'Cocktail'][is_cocktail]}"
+
+
+def categorize_dish(dish_name: str, dish_ingredients: set[str]) -> str:
+    """Categorize `dish_name` based on `dish_ingredients`.
+
+    :param dish_name: str - dish to be categorized.
+    :param dish_ingredients: list - ingredients for the dish.
+    :return: str - the dish name appended with ": <CATEGORY>".
+
+    This function should return a string with the `dish name: <CATEGORY>` (which meal category the dish belongs to).
+    `<CATEGORY>` can be any one of  (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
+    All dishes will "fit" into one of the categories imported from `sets_categories_data.py`
+
+    """
+
+    def is_dish(category: set[str]) -> bool:
+        return all(ingredient in category for ingredient in dish_ingredients)
+
+    if is_dish(VEGAN):
+        category = "VEGAN"
+    elif is_dish(VEGETARIAN):
+        category = "VEGETARIAN"
+    elif is_dish(PALEO):
+        category = "PALEO"
+    elif is_dish(KETO):
+        category = "KETO"
+    elif is_dish(OMNIVORE):
+        category = "OMNIVORE"
+    else:
+        raise ValueError(f"Cannot categorise: {dish_name}")
+
+    return f"{dish_name}: {category}"
+
+
+def tag_special_ingredients(dish: tuple[str, list[str]]) -> tuple[str, set[str]]:
+    """Compare `dish` ingredients to `SPECIAL_INGREDIENTS`.
+
+    :param dish: tuple - of (dish name, list of dish ingredients).
+    :return: tuple - containing (dish name, dish special ingredients).
+
+    Return the dish name followed by the `set` of ingredients that require a special note on the dish description.
+    For the purposes of this exercise, all allergens or special ingredients that need to be tracked are in the
+    SPECIAL_INGREDIENTS constant imported from `sets_categories_data.py`.
+    """
+
+    special_ingredients = set(filter(lambda i: i in SPECIAL_INGREDIENTS, dish[1]))
+    return dish[0], special_ingredients
+
+
+def compile_ingredients(dishes: list[set[str]]) -> set[str]:
+    """Create a master list of ingredients.
+
+    :param dishes: list - of dish ingredient sets.
+    :return: set - of ingredients compiled from `dishes`.
+
+    This function should return a `set` of all ingredients from all listed dishes.
+    """
+
+    return set(ingredient for dish in dishes for ingredient in dish)
+
+
+def separate_appetizers(dishes: list[str], appetizers: list[str]) -> list[str]:
+    """Determine which `dishes` are designated `appetizers` and remove them.
+
+    :param dishes: list - of dish names.
+    :param appetizers: list - of appetizer names.
+    :return: list - of dish names that do not appear on appetizer list.
+
+    The function should return the list of dish names with appetizer names removed.
+    Either list could contain duplicates and may require de-duping.
+    """
+
+    return list(set(dishes) - set(appetizers))
+
+
+def singleton_ingredients(dishes: list[set[str]], _: set[str]) -> set[str]:
+    """Determine which `dishes` have a singleton ingredient (an ingredient that only appears once across dishes).
+
+    :param dishes: list - of ingredient sets.
+    :param intersection: constant - can be one of `<CATEGORY>_INTERSECTIONS` constants imported from `sets_categories_data.py`.
+    :return: set - containing singleton ingredients.
+
+    Each dish is represented by a `set` of its ingredients.
+
+    Each `<CATEGORY>_INTERSECTIONS` is an `intersection` of all dishes in the category. `<CATEGORY>` can be any one of:
+        (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
+
+    The function should return a `set` of ingredients that only appear in a single dish.
+    """
+
+    # How to use second function parameter `intersection`? 😅
+    all_ingredients = compile_ingredients(dishes)
+
+    return {
+        ingredient
+        for ingredient in all_ingredients
+        if sum(ingredient in dish for dish in dishes) == 1
+    }
