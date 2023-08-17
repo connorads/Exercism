79047795f9e045c21810ebcafa374ce7492b4527
Connor Adams <connorads@users.noreply.github.com>
2023-08-17 21:47:39 +0200

    Revert "EllensAlienGame dynamic methods"
    This reverts commit df2281805b038b12d4adb05b3a0e4c6661b84c7c.

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,7 +1,6 @@
 """Convert seconds to years on different planets in the solar system."""
 
-from types import MethodType
-from typing import Callable, Literal, Self
+from typing import Literal
 
 Planet = Literal[
     "earth", "mercury", "venus", "mars", "jupiter", "saturn", "uranus", "neptune"
@@ -26,9 +25,37 @@ class SpaceAge:
     def __init__(self, seconds: int):
         self.seconds = seconds
 
-        for planet, _ in SECONDS_IN_PLANET_YEAR.items():
-            method = self._on_planet(planet)
-            setattr(self, "on_" + planet, MethodType(method, self))
+    def _on_planet(self, planet: Planet) -> float:
+        return round(self.seconds / SECONDS_IN_PLANET_YEAR[planet], 2)
 
-    def _on_planet(self, planet: Planet) -> Callable[[Self], float]:
-        return lambda self: round(self.seconds / SECONDS_IN_PLANET_YEAR[planet], 2)
+    def on_earth(self) -> float:
+        """Return the age in Earth years"""
+        return self._on_planet("earth")
+
+    def on_mercury(self) -> float:
+        """Return the age in Mercurary years"""
+        return self._on_planet("mercury")
+
+    def on_venus(self) -> float:
+        """Return the age in Venus years"""
+        return self._on_planet("venus")
+
+    def on_mars(self) -> float:
+        """Return the age in Mars years"""
+        return self._on_planet("mars")
+
+    def on_jupiter(self) -> float:
+        """Return the age in Jupiter years"""
+        return self._on_planet("jupiter")
+
+    def on_saturn(self) -> float:
+        """Return the age in Saturn years"""
+        return self._on_planet("saturn")
+
+    def on_uranus(self) -> float:
+        """Return the age in Uranus years"""
+        return self._on_planet("uranus")
+
+    def on_neptune(self) -> float:
+        """Return the age in Neptune years"""
+        return self._on_planet("neptune")
